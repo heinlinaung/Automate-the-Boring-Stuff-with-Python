6a68e265be6bbf80453b4feac404d3bb6075335a
Hein Lin Aung <heinlinaung6891@gmail.com>
2025-06-01 15:46:28 +0700

    added readme and progress

---
+++ lessons/01-basics.py
@@ -0,0 +1,11 @@
+# this program says hello and asks for my name
+print("Hello world")
+print("what is your name?")
+myName = input()
+print("Nice to meet you,"+myName)
+print("The length of the name is ", len(myName))
+
+## ask for age
+print("How old are you?")
+myAge = input()
+print("You will be ",str(int(myAge)+1), " in next year.")
